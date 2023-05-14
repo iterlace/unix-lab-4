1f9e1e0e2b33fc084c94c6a8f464b07be3eef983
iterlace <iterlace.dev@sent.com>
2023-05-14 22:23:26 +0300

    completed in a non-highly-scalable way

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, MutexGuard};
 use std::thread;
 use std::time::Duration;
 use rand::Rng;
 
 const PHILOSOPHERS_COUNT: u8 = 5;
-const THINKING_DURATION: u8 = 5;  // in seconds
+const THINKING_DURATION: u8 = 4;  // in seconds
+const EATING_DURATION: u8 = 5;
 
 //
 //      o2  —3—  o3
@@ -25,34 +26,53 @@ fn right_fork(i: u8) -> u8 {
     return (i+1) % PHILOSOPHERS_COUNT;
 }
 
-
-fn take_forks(philosopher_id: u8, control_mutex: Arc<Mutex<()>>, fork_mutexes: Arc<Vec<Mutex<()>>>) {
-    let control_mutex_guard = control_mutex.lock().unwrap();
-    let left = &fork_mutexes.clone()[left_fork(philosopher_id) as usize];
-    let right = &fork_mutexes.clone()[right_fork(philosopher_id) as usize];
-    loop {
-        left.try_lock().and_then(|| right.try_lock())
-    }
-    println!("{:?} has acquired a control_mutex", thread::current().name());
+fn log(msg: &str, philosopher_id: u8) {
+    let whitespaces = " ".repeat((philosopher_id * 4) as usize);
+    println!("{}{} {}", whitespaces, philosopher_id, msg);
 }
 
-fn put_forks_back(philosopher_id: u8, control_mutex: Arc<Mutex<()>>, fork_mutexes: Arc<Vec<Mutex<()>>>) {
-    let control_mutex_guard = control_mutex.lock().unwrap();
-    println!("{:?} has acquired a control_mutex", thread::current().name());
+fn take_forks_and_eat(philosopher_id: u8, control_mutex: Arc<Mutex<()>>, fork_mutexes: Arc<Vec<Mutex<()>>>) {
+    let left = &fork_mutexes.clone()[left_fork(philosopher_id) as usize];
+    let right = &fork_mutexes.clone()[right_fork(philosopher_id) as usize];
 
+    let mut left_mutex_guard: Option<MutexGuard<()>> = None;
+    let mut right_mutex_guard: Option<MutexGuard<()>> = None;
+
+    {
+        // wait for control_mutex, and then acquire it
+        log("is waiting for control_mutex", philosopher_id);
+        let control_mutex_guard = control_mutex.lock().unwrap();
+        log("has acquired a control_mutex", philosopher_id);
+
+        while left_mutex_guard.is_none() && right_mutex_guard.is_none() {
+            left.try_lock()
+                .and_then(|lmg| right.try_lock()
+                    .and_then(|rmg| {
+                        left_mutex_guard = Some(lmg);
+                        right_mutex_guard = Some(rmg);
+                        log("has acquired both forks", philosopher_id);
+                        Ok(())
+                    })
+                )
+                .unwrap_or_else(|_| ());
+        }
+        log("has released a control_mutex", philosopher_id);
+    }
+
+    log("has started eating their meal", philosopher_id);
+    thread::sleep(Duration::from_secs(EATING_DURATION as u64));
+    log("has finished eating their meal", philosopher_id);
 }
 
 fn think(philosopher_id: u8) -> () {
-    println!("{} is thinking for {} seconds...", thread::current().name().unwrap(), THINKING_DURATION);
+    log(format!("is thinking for {} seconds...", THINKING_DURATION).as_str(), philosopher_id);
     thread::sleep(Duration::from_secs(THINKING_DURATION as u64));
 }
 
 fn philosopher(philosopher_id: u8, control_mutex: Arc<Mutex<()>>, fork_mutexes: Arc<Vec<Mutex<()>>>) {
     loop {
         think(philosopher_id.clone());
-        take_forks(philosopher_id, control_mutex.clone(), fork_mutexes.clone());
-        eat();
-
+        take_forks_and_eat(philosopher_id, control_mutex.clone(), fork_mutexes.clone());
     }
 }
 
@@ -74,7 +94,7 @@ fn main() {
             thread::Builder::new()
                 .name(format!("Philosopher #{}", &i))
                 .spawn(move || philosopher(
-                    1,
+                    i,
                     control_mutex_clone,
                     fork_mutexes_clone
                 ))
